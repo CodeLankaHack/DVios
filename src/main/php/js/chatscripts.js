7a842043d17cad98fe2f37704a14e7cf92ec00d7
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2016-05-13 05:00:29 +0530

    More request types added

--- src/main/php/js/chatscripts.js
+++ src/main/php/js/chatscripts.js
@@ -59,31 +59,35 @@
         };
         processResponse = function (response) {
             setTimeout(function () {
-                setBotState(response['messageType']);
 
-                if(response['messageType']=='Comparision'){
-                    sendMessage(response['message'], true);
-                    $('#display-info').empty();
-                    $('#display-info').append("<div style='display: none;' class=\"panel panel-yellow\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Item</h3></div><div class=\"panel-body\">Product name: " + response['mainItem'] + "<br>Brand: " + response['brand'] + "<br>Price: Rs. " + response['price'] + "</div></div>");
-                    $('.panel-yellow').fadeIn(1000);
-                }
-                else if(response['messageType']=='Location'){
-                    sendMessage(response['message'], true);
-                }
-                else if(response['messageType']=='Price'){
-                    sendMessage(response['message'], true);
-                    $('#display-info').empty();
-                    $('#display-info').append("<div style='display: none;' class=\"panel panel-success\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Item</h3></div><div class=\"panel-body\">Product name: " + response['mainItem'] + "<br>Brand: " + response['brand'] + "<br>Price: Rs. " + response['price'] + "</div></div>");
-                    $('.panel-success').fadeIn(1000);
-                }
-                else if(response['messageType']=='PriceList'){
-                    $('#display-info').empty();
-                    var i=0;
-                    for(i=0;i<response['related'].length;i++){
-                        $('#display-info').append("<div style='display: none;' class=\"panel panel-info\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Item</h3></div><div class=\"panel-body\">Product name: " + response['related'][i]['mainItem'] + "<br>Brand: " + response['related'][i]['brand'] + "<br>Price: Rs. " + response['related'][i]['price'] + "</div></div>");
-                        $('.panel-info').fadeIn(1000);
+                if(response!=null && response['error']==0){
+                    setBotState(response['messageType']);
+                    if(response['messageType']=='Comparision'){
+                        sendMessage(response['message'], true);
+                        $('#display-info').empty();
+                        $('#display-info').append("<div style='display: none;' class=\"panel panel-yellow\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Item</h3></div><div class=\"panel-body\">Product name: " + response['mainItem'] + "<br>Brand: " + response['brand'] + "<br>Price: Rs. " + response['price'] + "</div></div>");
+                        $('.panel-yellow').fadeIn(1000);
+                    }
+                    else if(response['messageType']=='Location'){
+                        sendMessage(response['message'], true);
+                    }
+                    else if(response['messageType']=='Price'){
+                        sendMessage(response['message'], true);
+                        $('#display-info').empty();
+                        $('#display-info').append("<div style='display: none;' class=\"panel panel-success\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Item</h3></div><div class=\"panel-body\">Product name: " + response['mainItem'] + "<br>Brand: " + response['brand'] + "<br>Price: Rs. " + response['price'] + "</div></div>");
+                        $('.panel-success').fadeIn(1000);
+                    }
+                    else if(response['messageType']=='PriceList'){
+                        $('#display-info').empty();
+                        var i=0;
+                        for(i=0;i<response['related'].length;i++){
+                            $('#display-info').append("<div style='display: none;' class=\"panel panel-info\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Item</h3></div><div class=\"panel-body\">Product name: " + response['related'][i]['mainItem'] + "<br>Brand: " + response['related'][i]['brand'] + "<br>Price: Rs. " + response['related'][i]['price'] + "</div></div>");
+                            $('.panel-info').fadeIn(1000);
+                        }
                     }
                 }
+                else{
+                    sendMessage('Sorry I cannot understand...', true);                }
             }, 1000);
 
         };
